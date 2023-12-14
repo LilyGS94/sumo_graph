7d8e6b7825b354ad315ecad8158a719bf1fe25d2
LilyGS94 <77353360+LilyGS94@users.noreply.github.com>
2023-12-14 17:02:09 +0000

    update pulling data

--- pulling_data.py
+++ pulling_data.py
@@ -1,32 +1,45 @@
+import os
+
 import requests
 import logging
 import json
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 
+from requests.adapters import HTTPAdapter
+
+
 class SumoApiQuery:
-    def __init__(self, timestamps):
+    def __init__(self, timestamps, pool_size=20):
         self.timestamps = timestamps
         self.base_url = 'https://www.sumo-api.com/api/basho/{}/banzuke/Makuuchi'
-        self.results = {}
         self.session = requests.Session()
+        adapter = HTTPAdapter(pool_connections=pool_size, pool_maxsize=pool_size)
+        self.session.mount('https://', adapter)
+        self.session.mount('http://', adapter)
+        # Create a directory named with the current timestamp
+        self.output_dir = datetime.now().strftime("%Y%m")
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
 
     def query_endpoint(self, timestamp):
         url = self.base_url.format(timestamp)
+        logging.info(f"Making API call for timestamp: {timestamp}")
         try:
             response = self.session.get(url)
-            if response.status_code == 200:
-                self.results[timestamp] = response.json()
-                logging.info(f'Successfully fetched data for {timestamp}')
-            else:
-                self.results[timestamp] = None
-                logging.warning(f'Failed to fetch data for {timestamp}, status code: {response.status_code}')
+            response.raise_for_status()
+            response_data = response.json()
+            # Save the file in the new directory
+            with open(os.path.join(self.output_dir, f'{timestamp}.json'), 'w') as file:
+                json.dump(response_data, file)
+            logging.info(f"API call successful for timestamp: {timestamp}")
         except requests.RequestException as e:
-            logging.error(f'Error during request for {timestamp}: {e}')
-            self.results[timestamp] = None
+            logging.error(f"Error fetching data for {timestamp}: {e}")
 
-    def get_results(self):
-        return self.results
+    def run_queries(self):
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.query_endpoint, self.timestamps)
+
 
 def generate_timestamps():
     current_year = datetime.now().year
@@ -46,27 +59,29 @@ def setup_logging():
                         filename='sumo_api_query.log',
                         filemode='w')
 
-def save_data_to_file(data, filename="sumo_data.json"):
-    with open(filename, "w") as file:
-        json.dump(data, file, indent=4)
-    logging.info(f"Data successfully saved to {filename}")
+# def save_data_to_file(data, filename="sumo_data.json"):
+#     with open(filename, "w") as file:
+#         json.dump(data, file, indent=4)
+#     logging.info(f"Data successfully saved to {filename}")
 
 if __name__ == "__main__":
     setup_logging()
-
     timestamps = generate_timestamps()
     print(f"Generated {len(timestamps)} timestamps to query.")
 
-    sumo_api_query = SumoApiQuery(timestamps)
+    query = SumoApiQuery(timestamps)
+    query.run_queries()
 
-    print("Starting API queries...")
-    with ThreadPoolExecutor(max_workers=10) as executor:  # Adjust max_workers as needed
-        executor.map(sumo_api_query.query_endpoint, timestamps)
+    #sumo_api_query = SumoApiQuery(timestamps)
 
-    data = sumo_api_query.get_results()
+    # print("Starting API queries...")
+    # with ThreadPoolExecutor(max_workers=10) as executor:  # Adjust max_workers as needed
+    #     executor.map(sumo_api_query.query_endpoint, timestamps)
+    #
+    # data = sumo_api_query.get_results()
 
-    print("Saving data to file...")
-    save_data_to_file(data)
+    #print("Saving data to file...")
+    #save_data_to_file(data)
 
     print("Process completed.")
 
