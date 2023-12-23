c67887f860490f7f57b29a21470eb6001089e36f
LilyGS94 <77353360+LilyGS94@users.noreply.github.com>
2023-12-23 12:15:48 +0000

    begin work to create base api class

---
+++ base_classes.py
@@ -0,0 +1,52 @@
+import os
+
+import requests
+import logging
+import json
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+
+from requests.adapters import HTTPAdapter
+
+
+class SumoApiQuery:
+    def __init__(self, iters=None, pool_size=20):
+        self.log_file_name = "sumo_api_query_basho.log"
+        self.iters = iters
+        self.base_url = 'https://www.sumo-api.com/api/basho/{}/banzuke/Makuuchi'
+        self.session = requests.Session()
+        adapter = HTTPAdapter(pool_connections=pool_size, pool_maxsize=pool_size)
+        self.session.mount('https://', adapter)
+        self.session.mount('http://', adapter)
+        # Create a directory named with the current timestamp
+        self.now = datetime.now().strftime("%Y%m")
+        self.output_dir = f"data/{self.now}/basho"
+        # if self.endpoint == "rikishi":
+        #     self.base_url = "https://www.sumo-api.com/api/rikishi/{}"
+        #     self.output_dir = f"data/{self.now}/rikishi"
+        if not os.path.exists(self.output_dir):
+            os.makedirs(self.output_dir)
+
+    def query_endpoint(self, iter_val):
+        url = self.base_url.format(iter_val)
+        logging.info(f"Making API call for: {iter_val}")
+        try:
+            response = self.session.get(url)
+            response.raise_for_status()
+            response_data = response.json()
+            # Save the file in the new directory
+            with open(os.path.join(self.output_dir, f'{iter_val}.json'), 'w') as file:
+                json.dump(response_data, file)
+            logging.info(f"API call successful for: {iter_val}")
+        except requests.RequestException as e:
+            logging.error(f"Error fetching data for {iter_val}: {e}")
+
+    def setup_logging(self):
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            filename=self.log_file_name,
+                            filemode='w')
+    def run_queries(self):
+        self.setup_logging()
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.query_endpoint, self.iters)
--- pulling_data.py
+++ pulling_data.py
@@ -1,72 +1,28 @@
-import os
-
-import requests
-import logging
-import json
 from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor
 
-from requests.adapters import HTTPAdapter
+from base_classes import SumoApiQuery
 
+class SumoApiQueryBasho(SumoApiQuery):
+    def __init__(self):
+        super().__init__()
 
-class SumoApiQuery:
-    def __init__(self, timestamps, pool_size=20):
-        self.timestamps = timestamps
-        self.base_url = 'https://www.sumo-api.com/api/basho/{}/banzuke/Makuuchi'
-        self.session = requests.Session()
-        adapter = HTTPAdapter(pool_connections=pool_size, pool_maxsize=pool_size)
-        self.session.mount('https://', adapter)
-        self.session.mount('http://', adapter)
-        # Create a directory named with the current timestamp
-        self.now = datetime.now().strftime("%Y%m")
-        self.output_dir = f"data/{self.now}/basho"
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
-
-    def query_endpoint(self, timestamp):
-        url = self.base_url.format(timestamp)
-        logging.info(f"Making API call for timestamp: {timestamp}")
-        try:
-            response = self.session.get(url)
-            response.raise_for_status()
-            response_data = response.json()
-            # Save the file in the new directory
-            with open(os.path.join(self.output_dir, f'{timestamp}.json'), 'w') as file:
-                json.dump(response_data, file)
-            logging.info(f"API call successful for timestamp: {timestamp}")
-        except requests.RequestException as e:
-            logging.error(f"Error fetching data for {timestamp}: {e}")
-
-    def run_queries(self):
-        with ThreadPoolExecutor() as executor:
-            executor.map(self.query_endpoint, self.timestamps)
-
-
-def generate_timestamps():
-    current_year = datetime.now().year
-    current_month = datetime.now().month
-
-    timestamps = []
-    for year in range(1958, current_year + 1):
-        for month in ['01', '03', '05', '07', '09', '11']:
-            if year == current_year and int(month) > current_month:
-                break
-            timestamps.append(f'{year}{month}')
-    return timestamps
-
-
-def setup_logging():
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s',
-                        filename='sumo_api_query_basho.log',
-                        filemode='w')
+    def generate_timestamps(self):
+        current_year = datetime.now().year
+        current_month = datetime.now().month
+        timestamps = []
+        for year in range(1958, current_year + 1):
+            for month in ['01', '03', '05', '07', '09', '11']:
+                if year == current_year and int(month) > current_month:
+                    break
+                timestamps.append(f'{year}{month}')
+        self.iters = timestamps
 
 
 if __name__ == "__main__":
-    setup_logging()
-    timestamps = generate_timestamps()
-    print(f"Generated {len(timestamps)} timestamps to query.")
-    query = SumoApiQuery(timestamps)
+    #setup_logging()
+    query = SumoApiQueryBasho()
+    query.generate_timestamps()
+    print(f"Generated {len(query.iters)} timestamps to query.")
     query.run_queries()
     print("Process completed.")
 
